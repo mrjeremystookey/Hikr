738c88c4259f63250531bc89f98eac0fbafdd265
Jeremy Stookey <mrjeremystookey@gmail.com>
2018-06-09 12:06:59 -0600

    Initial commit

---
+++ app/src/main/java/r/stookey/hikr/Entry.kt
@@ -0,0 +1,41 @@
+package r.stookey.hikr
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import org.jetbrains.anko.toast
+
+
+
+
+class Entry: AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.entry)
+        val btnSignUp: Button = findViewById(R.id.bSignup)
+        btnSignUp.setOnClickListener(this)
+        val btnLogin: Button = findViewById(R.id.bLogin)
+        btnLogin.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?){
+        when (v?.id) {
+            R.id.bLogin -> {
+                toast("Login started")
+                val loginIntent = Intent(this, Login::class.java)
+                startActivity(loginIntent)
+
+            }
+            R.id.bSignup -> {
+                toast("Signup started")
+                val signUpIntent = Intent(this, Signup::class.java)
+                startActivity(signUpIntent)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/r/stookey/hikr/Signup.kt
@@ -0,0 +1,11 @@
+package r.stookey.hikr
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Signup: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup)
+    }
+}
