6e499602579bfdce85e1a38f76b616c713457b11
Jeremy Stookey <mrjeremystookey@gmail.com>
2018-06-15 07:59:14 -0600

    layout for a post is created, fab is redrawn on keyboard expansion.

--- app/src/main/java/r/stookey/hikr/Post.kt
+++ app/src/main/java/r/stookey/hikr/Post.kt
@@ -1,13 +1,13 @@
 package r.stookey.hikr
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.post.*
 import org.jetbrains.anko.toast
 
-class Post: AppCompatActivity() {
+class Post: AppCompatActivity(), View.OnClickListener {
 
     val TAG: String = "POST"
 
@@ -21,6 +21,17 @@ class Post: AppCompatActivity() {
         createdUser = intent.getStringExtra("createdUser")
         Log.d(TAG, createdUser)
         //tvUsername.text = createdUser
+        fabAddPost.setOnClickListener(this)
+
+
+    }
+
+    override fun onClick(v: View?) {
+        toast("Action Button Pressed")
+    }
+
+    data class Post(val id: String, val user: String, val location: Double, val time: Int, var message: String){
+        
 
     }
 }
--- app/src/main/java/r/stookey/hikr/Signup.kt
+++ app/src/main/java/r/stookey/hikr/Signup.kt
@@ -6,18 +6,14 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.signup.*
 import org.jetbrains.anko.toast
 
 class Signup: AppCompatActivity(), View.OnClickListener{
     var TAG: String = "Signup"
 
-    lateinit var username: EditText
-    lateinit var email: EditText
-    lateinit var password: EditText
-    lateinit var passwordConfirm: EditText
     lateinit var next: Button
     lateinit var createdUser: User
 
@@ -29,26 +25,21 @@ class Signup: AppCompatActivity(), View.OnClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup)
-
-        username = findViewById(R.id.etUsername)
-        email = findViewById(R.id.etEmail)
-        password = findViewById(R.id.etPassword)
-        passwordConfirm = findViewById(R.id.etPasswordConfirm)
-
         next = findViewById(R.id.bNext)
         next.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
+
         Log.d(TAG, "creating new user...")
         createNewUser()
     }
 
     fun createNewUser(){
-        if(password.text.toString() == (passwordConfirm.text.toString())) {
+        if(etPassword.text.toString() == (etPasswordConfirm.text.toString())) {
             ref = FirebaseDatabase.getInstance().getReference("Users")
             userId = ref.push().key
-            createdUser = User(userId, username.text.toString(), email.text.toString(), password.text.toString())
+            createdUser = User(userId, etUsername.text.toString(), etEmail.text.toString(), etPassword.text.toString())
             ref.child(userId).setValue(createdUser).addOnCompleteListener {
                 Log.d(TAG, "user creation successful")
                 toast("user creation successful")
