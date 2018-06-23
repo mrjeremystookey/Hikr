bad877f3d8f2fe6d72b3b6098f2cc5ba6689c8fb
Jeremy Stookey <mrjeremystookey@gmail.com>
2018-06-23 00:19:39 -0600

    post fragment and UI layout creation

---
+++ app/src/main/java/r/stookey/hikr/Message.kt
@@ -0,0 +1,9 @@
+package r.stookey.hikr
+
+data class Message(val data: Long, val Location: String, val text: String){
+
+
+
+
+
+}
--- app/src/main/java/r/stookey/hikr/Post.kt
+++ app/src/main/java/r/stookey/hikr/Post.kt
@@ -1,13 +1,16 @@
 package r.stookey.hikr
 
+
+import android.app.FragmentManager
 import android.os.Bundle
+
+
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.post.*
 import org.jetbrains.anko.toast
-import r.stookey.hikr.R.id.fabAddPost
-import r.stookey.hikr.R.id.tvUsername
+
 
 //setting up the bottom nav bar
 //https://medium.com/@hitherejoe/exploring-the-android-design-support-library-bottom-navigation-drawer-548de699e8e0
@@ -15,6 +18,10 @@ import r.stookey.hikr.R.id.tvUsername
 class Post: AppCompatActivity(), View.OnClickListener {
 
     private val TAG: String = "POST"
+
+    private lateinit var textString: String
+    private lateinit var message: Message
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +31,7 @@ class Post: AppCompatActivity(), View.OnClickListener {
         fabAddPost.setOnClickListener(this)
     }
 
+
     override fun onStart() {
         super.onStart()
         determineUser()
@@ -31,20 +39,25 @@ class Post: AppCompatActivity(), View.OnClickListener {
 
     private fun determineUser() {
         if (intent.hasExtra("email") && intent.hasExtra("username")) {
-            tvUsername.text = intent.getStringExtra("username")
-            Log.d(TAG, "determineUser(): logged in as " + tvUsername.text)
+           // tvUsername.text = intent.getStringExtra("username")
+           // Log.d(TAG, "determineUser(): logged in as " + tvUsername.text)
         } else if (intent.hasExtra("newUserEmail") && intent.hasExtra("newUserUsername")) {
-            tvUsername.text = intent.getStringExtra("newUserUsername")
-            Log.d(TAG, "determineUser(): new user registered, username is " + tvUsername.text)
+           // tvUsername.text = intent.getStringExtra("newUserUsername")
+           //  Log.d(TAG, "determineUser(): new user registered, username is " + tvUsername.text)
         }
     }
 
     override fun onClick(v: View?) {
         //TODO create new post for logged in user
+        var postFragment = PostFragment.newInstance()
+        //val fragmentTransaction = fragmentManager.beginTransaction()
+        //fragmentTransaction.commit()
+        toast(postFragment.getText())
     }
 
-//    data class Post(val id: String, val user: String, val location: Double, val time: Int, var message: String){
-//
-//    }
+
+
+
+
 
 }
